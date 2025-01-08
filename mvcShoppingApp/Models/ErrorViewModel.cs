35d8335b012bd764ee7f7d1d493b47dff71c70c8
Rekha Nagaraju (UST Global Inc) <v-rnagaraju@microsoft.com>
2025-01-08 15:05:00 +0530

    Added files

---
+++ mvcShoppingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mvcShoppingApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
